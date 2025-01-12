5a6dc23a01814816de17e7d31dbda257103a4d30
nathanielpierson <nathanielepierson@gmail.com>
2025-01-12 11:05:14 -0500

    add response if content is nill

--- reddit.rb
+++ reddit.rb
@@ -1,20 +1,27 @@
 require 'http'
+require 'tty-prompt'
+prompt = TTY::Prompt.new
+
 puts "Please enter the name of a subreddit"
 subreddit = gets.chomp
 begin
   response = HTTP.get"https://www.reddit.com/r/#{subreddit}/.json"
   link = response.parse(:json)
-  # pp link
-
+  # rescues thrown error resulting from an url that doesn't lead to a subreddit
   rescue
     puts "not a subreddit"
 end
 
+# sends the first 15 posts to terminal
 x = 2
-25.times do
-  puts "#{x-2}. #{link["data"]["children"][x]["data"]["title"]}"
+15.times do
+  puts "#{x-1}. #{link["data"]["children"][x]["data"]["title"]}"
+  # this line only works with certain subreddits. Need to fix that
+  if link["data"]["children"][x]["data"]["media_embed"]["content"] == nil
+    puts "(post has no text)"
+  else
+    puts "#{link["data"]["children"][x]["data"]["media_embed"]["content"]}"
+  end
   puts ""
-  puts "#{link["data"]["children"][x]["data"]["media_embed"]["content"]}"
   x += 1
 end
-# prework 454 was where we learned rescue
